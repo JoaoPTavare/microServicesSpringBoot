97acb42097e8fd3502f3312783f33e77fb574f2e
JoaoPTavare <jjoaopedrotava05@gmail.com>
2025-09-20 17:40:52 -0300

    Creat a new microservice project

---
+++ ms-worker/demo/src/main/java/ms_work/MsWorkApplication.java
@@ -0,0 +1,13 @@
+package ms_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWorkApplication.class, args);
+	}
+
+}
---
+++ ms-worker/demo/src/test/java/ms_work/MsWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ms_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
