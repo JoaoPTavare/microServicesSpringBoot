4d84143c20991a2531dfb7063d2b425bd54b7f1b
JoaoPTavare <jjoaopedrotava05@gmail.com>
2025-09-27 13:24:14 -0300

    Add pay roll ao projeto

---
+++ ms-payRoll/demo/src/main/java/hr_payRoll/demo/HrPayRollApplication.java
@@ -0,0 +1,13 @@
+package hr_payRoll.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayRollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayRollApplication.class, args);
+	}
+
+}
---
+++ ms-payRoll/demo/src/test/java/hr_payRoll/demo/HrPayRollApplicationTests.java
@@ -0,0 +1,13 @@
+package hr_payRoll.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayRollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
